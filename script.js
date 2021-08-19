5ee863c81c4338a5ea464f04c591c2e191eb0282
qazi112 <qarsalan13@gmail.com>
2021-08-19 23:56:42 +0500

    Code Complete- promises code added in script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+var userType = true;
+// Promises JS
+
+// Create a promise
+// method 1:
+// Promise takes a function with two parameters (resolve,reject) 
+
+var promise =  new Promise((resolve,reject)=>{
+    if(userType){
+        resolve('Success');
+    }
+    else{
+        reject('Failed')
+    }
+})
+
+// Create a promise
+// method 2:
+// Promise takes a function with two parameters (resolve,reject) 
+function userChecker(){
+    return new Promise((resolve,reject)=>{
+        if(userType)
+        {
+            resolve('Success');
+        }
+        else
+        {
+            reject('Failed')
+        }
+    })
+}
+
+window.onload = ()=>{
+
+    // method 1:
+    promise.then((success)=>{
+        console.log(success)
+
+    }).catch((error)=>{
+        console.log(error)
+    })
+    
+    // method 2:
+    userChecker().then
+
+}
+
+
