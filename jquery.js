2e2d17dc9bb2b61ae002e9fdfdf3a8df02d71afc
qazi112 <qarsalan13@gmail.com>
2021-08-21 23:42:25 +0500

    added new file and changing few things!

---
+++ jquery.js
@@ -0,0 +1,16 @@
+$(function(){
+    var dis = true;
+
+    console.log('Get Ready');
+    // blur and focus functions are for input fields when get clicked and release focus
+    $("input").focus(function(){
+        $(this).css("background-color", "#cccccc");
+    });
+    $('input').blur(function(){
+        $(this).css('background-color','#ffffff');
+    });
+    // =============================
+    
+
+
+  });
